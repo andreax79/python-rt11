314311666112d9a6ac95a79b6df2b32ba5f93e61
Andrea Bonomi <andrea.bonomi@jakala.com>
2024-05-20 22:34:30 +0200

    Add CAPS-11 to the zoo

---
+++ zoo/caps11/caps11.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+# Set the current working directory to the directory of this script
+cd "$(dirname "$0")"
+
+BOOT_DISK="caps11.t60"
+URL="http://simh.trailing-edge.com/kits/caps11_system.zip"
+
+# Download the boot disk
+if [ ! -f "${BOOT_DISK}" ]; then
+    mkdir -p tmp
+    cd tmp
+    URL_REL=${URL:7}
+    URL_REL=${URL_REL#*/}
+    URL_REL="/${URL_REL%%\?*}"
+    FILENAME="${URL_REL##/*/}"
+    curl -LO ${URL}
+    unzip ${FILENAME}
+    mv ${BOOT_DISK} ..
+    cd ..
+    rm -rf tmp
+fi
+
+pdp11 pdp11.ini
